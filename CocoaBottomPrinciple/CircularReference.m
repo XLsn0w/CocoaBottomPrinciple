4bfba6831d2c29e2da468fa679c1af635508f8fb
xlsn0w <xlsn0wios@gmail.com>
2018-05-21 18:35:32 +0800

    update

--- CocoaBottomPrinciple/CircularReference.m
+++ CocoaBottomPrinciple/CircularReference.m
@@ -17,6 +17,23 @@
 }
 @end
 
+/*
+ 即使在你的block代码中没有显式地出现"self"，也会出现循环引用！只要你在block里用到了self所拥有的东西！但对于这种情况，我们无法通过加__weak声明或者__block声明去禁止block对self进行强引用或者强制增加引用计数。但我们可以通过其他指针来避免循环引用(多谢xq_120的提醒)，具体是这么做的：
+ 
+ 
+ __weak typeof(self) weakSelf = self;
+ self.blkA = ^{
+ __strong typeof(weakSelf) strongSelf = weakSelf;//加一下强引用，避免weakSelf被释放掉
+ NSLog(@"%@", strongSelf->_xxView); //不会导致循环引用.
+ 
+ 
+ self->_timer
+ 
+ 
+ };
+ 
+ */
+
 @implementation CircularReference
 
 - (instancetype)init {
